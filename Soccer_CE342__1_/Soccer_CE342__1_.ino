c746582383d1ce7da1a8f303be13304c4f5ead1b
MindsRobotic <minds.roboticfll@gmail.com>
2025-08-31 13:17:02 -0300

    Update Soccer_CE342__1_.ino

--- Soccer_CE342__1_/Soccer_CE342__1_.ino
+++ Soccer_CE342__1_/Soccer_CE342__1_.ino
@@ -37,8 +37,6 @@ float proporcional, integral, derivativo = 0;
 // --- Velocidade ---
 int velocidade = 255;
 
-// --- Bússola ---
-Adafruit_BNO055 bno = Adafruit_BNO055();
 
 // --- Média móvel IR ---
 #define BUFFER_SIZE 5
@@ -62,13 +60,6 @@ void setup() {
   // Inicializa IR
   InfraredSeeker::Initialize();
 
-  // Inicializa BNO055
-  if (!bno.begin()) {
-    Serial.println("Erro ao iniciar BNO055!");
-    while (1);
-  }
-  bno.setExtCrystalUse(true);
-
   // Motores
   pinMode(M1_ENA, OUTPUT); pinMode(M1_IN1, OUTPUT); pinMode(M1_IN2, OUTPUT);
   pinMode(M2_ENB, OUTPUT); pinMode(M2_IN3, OUTPUT); pinMode(M2_IN4, OUTPUT);
@@ -85,31 +76,6 @@ void parar() {
   analogWrite(M1_ENA, 0);
   analogWrite(M2_ENB, 0);
   analogWrite(M3_ENA, 0);
-}
-
-
-void Bussola(float erroBussola, int velocidade) {
-  float correcao_Bussola = kp * erroBussola; // Limitando correção para ficar suave e ñ ir duma vez
-
-  int velEsq, velDir, velTras = 0;
-
-   if (erroBussola == 0) {
-    velEsq  = constrain(velocidade - correcao_Bussola, 0, 255);
-    velDir  = constrain(velocidade + correcao_Bussola, 0, 255) * -1;
-    velTras = constrain(correcao_Bussola, 0, 255);
-  }
-  if (erroBussola < 3) {
-    velEsq  = constrain(velocidade - correcao_Bussola, 0, 255) * -1;
-    velDir  = constrain(velocidade + correcao_Bussola, 0, 255) * -1;
-    velTras = constrain(correcao_Bussola, 0, 255) * -1;
-  }
-  if (erroBussola > 3) {
-    velEsq  = constrain(velocidade - correcao_Bussola, 0, 255);
-    velDir  = constrain(velocidade + correcao_Bussola, 0, 255);
-    velTras = constrain(correcao_Bussola, 0, 255);
-  }
-
-  Deslocar(velEsq, velDir, velTras);
 }
 void PID(float erro, int velocidade) {
   proporcional = kp * erro;
@@ -187,28 +153,15 @@ void loop() {
   ballDirection = InfraredBall.Direction; // Armazena a direção na variável
   ballIntens = InfraredBall.Strength; // Armazena a direção na variável
 
-  int erro = 5 - ballDirection;  // centro do IR é 5
-
-  // Bússola
-  sensors_event_t event;
-  bno.getEvent(&event);
-  float compassAngle = event.orientation.x; // 0° é o gol adversário
-  // erro da bússola para 0°, normalizado para -180..180 para girar pelo menor lado
-  float erroBussola = -compassAngle;
-  if (erroBussola > 180)  erroBussola -= 360;
-  if (erroBussola < -180) erroBussola += 360;
-
-  // Escolha da estratégia
-  if (ballIntens > 220) {     // bola perto: usa bússola para apontar pro gol
-    Bussola(erroBussola, velocidade);
-  } else {                    // bola longe: segue a bola pelo IR
+  int erro = 5 - ballDirection;  // centro do IR é 5                   // bola longe: segue a bola pelo IR
     PID(erro, velocidade);
-  }
+
 
   // Debug
   Serial.print("Dir IR: "); Serial.print(ballDirection);
   Serial.print(" | Intens: "); Serial.print(ballIntens);
   Serial.print(" | Compass: "); Serial.println(compassAngle);
 
-  delay(50);
+
 }
+
